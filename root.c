98b67f5150b067682799ea004f94381dec2c61dc
Phillip Stephan <phillipstephan314@gmail.com>
2023-12-11 16:09:03 +0100

    root fix

--- root.c
+++ root.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <math.h>
 int main(int argc, const char *argv[]){
-if(argc<=1 || atof(argv[1])<1){
+if(argc<=1 || atof(argv[1])<0){
     printf("ungueltiger Zahlenbereich");
     return 1;
 }
 double basis = atof(argv[1]);
 double ergebnis=basis;
-while(!(ergebnis*ergebnis-basis<basis*10e-10)){
+while(!(fabs(ergebnis*ergebnis-basis)<basis*10e-10)){
+printf("%.15g\n",ergebnis);
 ergebnis=(ergebnis+(basis/ergebnis))/2;
 }
 
-printf("%f",ergebnis);
+printf("%.15g",ergebnis);
     return 0;
 }
