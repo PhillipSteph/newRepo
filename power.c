5fd8e8ccdf0922ecdbe10689ce5f8e8499db2c05
Phillip Stephan <phillipstephan314@gmail.com>
2023-12-11 15:18:29 +0100

    Power Aufgabe 3 fertug

---
+++ power.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+float ergebnis;
+void berechnepotenz(float basis, int exponent);
+
+int main(int argc, const char *argv[]){
+
+switch(argc){
+    case 1: printf("zu wenig Argumente!");
+        break;
+    case 2: printf("zu wenig Argumente!");
+        break;
+    case 3: berechnepotenz(atof(argv[1]),atoi(argv[2]));
+        break;
+    default:
+        printf("zu viele Argumente!");
+        break;
+}
+
+return 0;
+}
+
+
+void berechnepotenz(float basis, int exponent){
+ergebnis= basis;
+    for(int i=1;i<exponent;++i){
+        ergebnis *= basis;
+    }
+    if(exponent==0){
+        ergebnis=1;
+    }
+    printf("%f ^ %d = %f",basis,exponent,ergebnis);
+}
