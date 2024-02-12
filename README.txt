2ea2a7695eb25fb20b566e2c211a9a54f07e089e
Phillip Stephan <phillipstephan314@gmail.com>
2024-02-12 21:29:06 +0100

    aufgabe 61 neu

---
+++ README.txt
@@ -0,0 +1,2 @@
+in übung22/aufgabe61 ist eine Definition für Pointer geschrieben.
+Garantiere für keine Richtigkeit in den Funktionen. 
--- übung22/aufgabe61.c
+++ übung22/aufgabe61.c
@@ -14,9 +14,10 @@ elem *neu(int zahl, elem *next)
 {
 
     elem *e = (elem *)(malloc(sizeof(elem)));
-        if(e==NULL){
-            fprintf(stderr,"Fehler bei Speicherreservierung: %s",strerror);
-        }
+    if (e == NULL)
+    {
+        fprintf(stderr, "Fehler bei Speicherreservierung: %s", strerror);
+    }
     e->anzahl = 0;
     e->zahl = zahl;
     e->next = next;
@@ -24,24 +25,63 @@ elem *neu(int zahl, elem *next)
     return e;
 }
 
-elem *find(elem* head,int zahl){
+elem *find(elem *first, int zahl)
+{
 
+    // Leer oder erstes ELement größer
+    // head zeigt auf kleinstes Element
+    // in sortierten Liste.
+    if (first->zahl >= zahl || first == NULL)
+    {
+        elem *n = neu(zahl, first);
+    }
+    else
+    {
+        elem *ptr = first;
+        while (ptr != NULL)
+        {
+            if(ptr->zahl == zahl){
+                return ptr;
+            }else if(ptr->next==NULL || ptr->next->zahl>=zahl){
+                elem* n=neu(zahl,ptr->next);
+                ptr->next = n;
+                return n;
+            }
+        ptr = ptr->next;
+        }
+    }
+    // legt neues ELement an, welches Auf vorheriges
+    // erstes Zeigt oder auf NULL wenn kein vorheriges existiert.
+}
+
+void print(elem* head){
+    elem* ptr = head;
+    while(ptr!=NULL){
+        printf("%d ",ptr->zahl);
+        ptr = ptr->next;
+    }
+    putchar('\n');
 }
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    elem* head = NULL;
+    for(int i=1;i<argc;i++){
+        elem* e = find(head,atoi(argv[i]));
+        e->anzahl++;
+
+    }
+    print(head);
     return 0;
 }
 
-
 // Deklaration:
 // Pointer auf einen Datentypen werden mit dem Datentypen selber und einem STernchen Deklariert
-//BSP: Ein Pointer auf einen Integer deklariert man     int* ptr;
-//Nach der DEklaration kann es zu Verwirrung führen, da man werte mit Pointern folgend ablesen kann:
+// BSP: Ein Pointer auf einen Integer deklariert man     int* ptr;
+// Nach der DEklaration kann es zu Verwirrung führen, da man werte mit Pointern folgend ablesen kann:
 
 // für eine Variable    int a = 10; kann man die Adresse lesen mit int* ptr = &a;
-// wenn man aber die Variable von einem Pointer lesen möchte so nutzt man 
+// wenn man aber die Variable von einem Pointer lesen möchte so nutzt man
 // int a = *ptr;
 // also:            Deklaration:    int* ptr; deklariert einen Pointer
 //                  Lesen:          int a = *ptr; liest wert von variable auf die der Pointer zeigt
